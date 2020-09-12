c6c22f599db11d67dc204bb86332e4b5c0226b3c
asilaaah <asilah2110@gmail.com>
2020-09-12 18:35:27 +0800

    create validation and store new products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,8 +6,30 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public function create()
     {
         return view('products.create');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'category'=> 'required',
+            'name' => 'required',
+            'description' => '',
+            'price' => 'required',
+            'quantity' => 'required',
+            'image' => 'required|image',
+        ]);
+
+        auth()->user()->products()->create($data);
+
+        dd(request()->all());
     }
 }
