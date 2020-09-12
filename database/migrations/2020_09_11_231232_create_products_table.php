a54e5c16d16239181bc3f5b49c76f402aed05612
asilaaah <asilah2110@gmail.com>
2020-09-12 08:06:46 +0800

    create products table

---
+++ database/migrations/2020_09_11_231232_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('category');
+            $table->string('name');
+            $table->string('image');
+            $table->text('description')->nullable();
+            $table->float('price',8,2);
+
+            $table->timestamps();
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
