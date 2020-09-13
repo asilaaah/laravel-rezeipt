71e35d80653c86e395a4443eb3480f57473a312c
asilaaah <asilah2110@gmail.com>
2020-09-14 00:25:37 +0800

    display added products in cashier's page

--- app/Http/Controllers/CashierController.php
+++ app/Http/Controllers/CashierController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Product;
 use Auth;
 use Illuminate\Http\Request;
 
@@ -8,6 +10,7 @@ class CashierController extends Controller
 {
     public function index()
     {
-        return view ('dashboard.cashier');
+        $products = Product::all();
+        return view('dashboard.cashier', compact('products'));
     }
 }
--- resources/views/dashboard/cashier.blade.php
+++ resources/views/dashboard/cashier.blade.php
@@ -2,22 +2,29 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    This is Cashier's Dashboard
-                </div>
+    <div class="row">
+        <div class="col-lg-12 margin-tb d-flex d-flex justify-content-between">
+            <div class="pull-left">
+                <h2>{{auth()->user()->name}} 's dashboard</h2>
             </div>
         </div>
     </div>
-</div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <div class="row pt-5">
+        @foreach($products as $product)
+            <div class="col-4 pb-4">
+                    <img src= "/storage/{{ $product->image }}" class="w-100">
+                    <div>{{ $product->name }}</div>
+                    <div>RM {{ number_format($product->price, 2, '.', ',') }}</div>
+            </div>
+        @endforeach
+    </div>
+   
+</div>   
 @endsection
