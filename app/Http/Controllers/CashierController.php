d40670db44252152740526223e01533f54308a52
asilaaah <asilah2110@gmail.com>
2020-09-15 16:43:50 +0800

    create categories navigation bar in cashier page

--- app/Http/Controllers/CashierController.php
+++ app/Http/Controllers/CashierController.php
@@ -2,16 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
+use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 
 class CashierController extends Controller
 {
     public function index()
     {
+        $user = Auth::user();
         $products = Product::all();
         
-        return view('dashboard.cashier', compact('products'));
+
+        if (request()->category) 
+        {
+            $products = Product::with('category')->whereHas('category', function ($query) {
+                $query->where('name', request()->category);
+            })->get();
+            $categories = Category::all();
+        } 
+        else {
+            $products = Product::all();
+            $categories = Category::all();
+        }
+        
+        return view('dashboard.cashier', compact('products', 'user', 'categories'));
     }
 }
--- resources/views/dashboard/cashier.blade.php
+++ resources/views/dashboard/cashier.blade.php
@@ -4,33 +4,36 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-12 margin-tb d-flex d-flex justify-content-between">
-            <div class="pull-left">
                 <h2>{{auth()->user()->name}} 's dashboard</h2>
+        </div>
+
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
             </div>
-        </div>
-    </div>
-   
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif 
-        <div class="row pt-5">
- 
-            @foreach($products as $product)
+        @endif 
+
+        <nav class="row">
+            <ul>
+                @foreach ($categories as $category)
+                <li><a href="{{ route('cashier.index', ['category' => $category->name]) }}">{{ $category->name }}</a></li>
+                @endforeach
+            </ul>
+        </nav>
+
+        <div class="row pl-5">
+            @forelse($products as $product)
             <div class="col-4 pb-4">
-                        <img src="/storage/{{ $product->image }}" class="w-100">
-                            <div>{{ $product->name }}</div>
-                            <div>{{ $product->description }}</div>
-                            <div><strong>Price: </strong> RM {{ number_format($product->price, 2, '.', ',') }}</div>
-                            <p class="btn-holder"><a href="#" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
-                           
+                <img src="/storage/{{ $product->image }}" class="w-100">
+                    <div>{{ $product->name }}</div>
+                    <div>{{ $product->description }}</div>
+                    <div><strong>Price: </strong> RM {{ number_format($product->price, 2, '.', ',') }}</div>
+                    <p class="btn-holder"><a href="#" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>         
             </div>
-        
-            @endforeach
- 
-        </div><!-- End row -->
- 
+            @empty
+            <div>No items found</div>
+            @endforelse
+        </div>
     </div>
 </div>
 @endsection
