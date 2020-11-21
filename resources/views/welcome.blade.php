291f7f918cc6ff9c97ff0237ed6fa18594c81e76
Sharifah Asilah <asilah2110@gmail.com>
2020-11-21 18:03:56 +0800

    add google adsense link in homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!--<script data-ad-client="ca-pub-9067141868740899" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>-->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
