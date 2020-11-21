d3380ea75558eb5876cff2e4e81f626b34b38ddf
Sharifah Asilah <asilah2110@gmail.com>
2020-11-21 18:03:19 +0800

    change admin password in AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -20,16 +20,14 @@ class AdminSeeder extends Seeder
             'id' => '1',
             'store_id' => "1",
             'name' => 'Admin',
-            'email' => 'admin@admin.com',
+            'email' => 'admin@rezeipt.online',
             'email_verified_at' => now(),
-            'password' => Hash::make('12345678'),
+            'password' => Hash::make('8w[?hBR:Ju2Gm'),
             'role' => 0,
             'admin' => 1,
             'approved_at' => now(),
         ]);
 
-        Profile::create(['user_id' => '1'
-        , 'id' => '1'
-        ]);
+        Profile::create(['user_id' => '1']);
     }
 }
