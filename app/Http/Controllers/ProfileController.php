56775862acb470b6745d9c56f553d294748c4d7b
Sharifah Asilah <asilah2110@gmail.com>
2020-11-25 23:57:39 +0800

    validate user age to be greater than 18 years old

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Profile;
 use App\Models\User;
+use DateTime;
+use Illuminate\Support\Facades\Validator;
 use FontLib\Table\Type\name;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
 {
@@ -26,18 +29,28 @@ class ProfileController extends Controller
 
     public function update(Profile $profile, User $user)
     {
+        Validator::extend('olderThan', function($attribute, $value, $parameters)
+        {
+            $minAge = ( ! empty($parameters)) ? (int) $parameters[0] : 13;
+            return (new DateTime())->diff(new DateTime($value))->y >= $minAge;
+
+            // or the same using Carbon:
+            // return Carbon\Carbon::now()->diff(new Carbon\Carbon($value))->y >= $minAge;
+        },'Age must be greater than 18 years old!');
+
         $this->authorize('update', $user->profile);
         $data = request()->validate([
             'phone_number' => '',
             'address' => '',
-            'birthday' => '',
+            'birthday' => 'nullable|olderThan:18',
             'profile_photo' => 'image'
         ]);
+
 
         if (request('profile_photo')){
             $imagePath = request('profile_photo')->store('profile', 'public');
 
-            $image = \Intervention\Image\Facades\Image::make(public_path("storage/{$imagePath}"))->fit(900, 900);
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(900, 900);
 
             $image->save();
 
@@ -57,6 +70,6 @@ class ProfileController extends Controller
 
         auth()->user()->update($details);
 
-        return redirect('/profile/'. $user->id);
+        return redirect('/profile/'. $user->id)->with('success','Profile updated successfully');
     }
 }
