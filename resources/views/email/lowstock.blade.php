a93613c7ae4b645fdb355e3a51ed82dbc68377da
Sharifah Asilah <asilah2110@gmail.com>
2020-12-21 11:43:08 +0800

    change header in low stock email

--- resources/views/email/lowstock.blade.php
+++ resources/views/email/lowstock.blade.php
@@ -1,5 +1,12 @@
-@component('mail::message', ['user'=>$user, 'product' => $product])
+@component('mail::layout', ['user'=>$user, 'product' => $product])
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+        Rezeipt
+        @endcomponent
+    @endslot
 
+{{-- Body --}}
 One of the product in inventory is in low stock. Please restock!
 <br>
 Product ID : {{$product->id}}
@@ -13,5 +20,11 @@ Minimum Quantity: {{$product->minimum_quantity}}
 @component('mail::button', ['url' => route('product.edit', $product->id)])
 Restock
 @endcomponent
-    
+
+{{-- Footer --}}
+@slot('footer')
+@component('mail::footer')
+    © {{ date('Y') }} Rezeipt. All rights reserved.
+@endcomponent
+@endslot
 @endcomponent
